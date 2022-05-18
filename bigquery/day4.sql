a6c436eb415ff88779322be593ed4abbe0e68a05
tatien777 <tientm0409@gmail.com>
2022-05-18 15:34:09 +0930

    all info

--- bigquery/day4.sql
+++ bigquery/day4.sql
@@ -43,3 +43,48 @@ CREATE TEMP TABLE covid_confirmed
   ;
   -- Update 
   DROP TABLE IF EXISTS covid_confirmed  ;
+
+  -- character 
+CREATE TEMPORARY FUNCTION FiveFirstChar(word STRING)
+  RETURNS STRING
+  AS (SUBSTR(word, 0, 5));
+
+SELECT 
+  FiveFirstChar(name) AS five_chars_name, 
+FROM  UNNEST(["David Ta"]) as name
+# using js 
+
+CREATE TEMPORARY FUNCTION js_FindFiveChars(word STRING)
+  RETURNS STRING
+  LANGUAGE js
+  AS "return word.substring(0, 5);";
+
+
+SELECT 
+  js_FindFiveChars(name) AS five_chars_name, 
+FROM  UNNEST(["David Ta"]) as name
+
+-- https://stackoverflow.com/questions/47795464/using-lead-in-bigquery
+
+
+DECLARE country STRING DEFAULT "Vietnam";
+DECLARE start_date datetime DEFAULT '2022-01-01';
+DECLARE end_date datetime DEFAULT '2022-01-31'; 
+
+## LEAD exaample 
+
+SELECT date ,confirmed as cummulative_confirmed
+,LEAD(confirmed, 1) OVER ( ORDER BY date) AS next_cummulative_day    
+FROM `bigquery-public-data.covid19_jhu_csse.summary`
+WHERE date between start_date and end_date
+AND country_region = country
+ORDER BY 2 
+
+## LAG exaample 
+SELECT date ,confirmed as cummulative_confirmed
+,LAG(confirmed, 1) OVER ( ORDER BY date) AS last_cummulative_day    
+,confirmed - IFNULL(LAG(confirmed, 1) OVER ( ORDER BY date),confirmed) AS new_confirmed    
+FROM `bigquery-public-data.covid19_jhu_csse.summary`
+WHERE date between start_date and end_date
+AND country_region = country
+ORDER BY 2 ;
